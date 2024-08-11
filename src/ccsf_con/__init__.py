2c773ead55af6c7b9f5f744d9ea5a9ebefb34653
clin94118 <clin@ccsf.edu>
2024-08-10 22:53:22 -0700

    feat: add code to __init__.py with test_init.py

--- src/ccsf_con/__init__.py
+++ src/ccsf_con/__init__.py
@@ -1,3 +1,49 @@
 # SPDX-FileCopyrightText: 2024-present clin94118 <clin@ccsf.edu>
 #
 # SPDX-License-Identifier: MIT
+# base imports
+import os
+import sys
+import time
+from datetime import datetime, timedelta
+
+# required imports
+from sqlalchemy import create_engine, text
+import pandas as pd
+import pyodbc
+import oracledb
+import cx_Oracle
+import requests
+
+def f_print_time():
+    """current date string with time
+
+    :return: current time format "%Y-%m-%d %H:%M:%S"
+    """
+    dt_string = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    return dt_string
+
+
+def f_split_df(in_df, in_nGrp):
+    """separates in_df into list of dataframes for improved processing
+
+    :param in_df: dataframe to process
+    :param in_nGrp: split number in each grouping
+    :return: list_df: list of dataframes
+    """
+    l_numReq = in_df.shape[0]
+    list_df = [in_df[i:i+in_nGrp] for i in range(0, l_numReq, in_nGrp)]
+
+    return list_df
+
+
+
+# import login information
+if sys.platform == 'linux':
+    l_login_path = '/home/' + os.getlogin() + '/.logins'
+else:
+    l_login_path = 'c:/Users/' + os.getlogin() + '/.logins'
+
+sys.path.append(l_login_path)
+import DB_LOGIN, IPQS_LOGIN
---
+++ tests/test_init.py
@@ -0,0 +1,25 @@
+import pytest
+from ccsf_con import f_print_time, f_split_df, DB_LOGIN
+from datetime import datetime
+import pandas as pd
+
+@pytest.fixture
+def my_df():
+    return pd.DataFrame({"foo_id": [1, 2, 3, 4, 5, 6]})
+
+def test_login_file():
+    assert DB_LOGIN.l_OCI_usr == 'baninst1'
+
+def test_f_print_time():
+    assert f_print_time() == datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def test_f_split_df(my_df):
+    test_grp = f_split_df(my_df, 3)
+
+    verify_df1 = pd.DataFrame({"foo_id": [1, 2, 3]})
+    verify_df2 = pd.DataFrame({"foo_id": [4, 5, 6]})
+    verify_df2 = verify_df2.set_index(pd.Series([3, 4, 5]))
+    verify_grp = [verify_df1, verify_df2]
+
+    for i in range(len(test_grp)):
+        pd.testing.assert_frame_equal(test_grp[i], verify_grp[i])

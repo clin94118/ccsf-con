fee5009ac245dda9585fe575ffa70cf926a352cd
clin94118 <clin@ccsf.edu>
2025-03-05 05:37:14 -0800

    refactor: use black on test_oracleDbCon.py

--- tests/test_oracleDbCon.py
+++ tests/test_oracleDbCon.py
@@ -2,9 +2,11 @@
 import pytest
 from ccsf_con import oracleDbCon as oracleDbCon
 
+
 @pytest.fixture
 def in_inst():
     return "OCI_PPRD"
+
 
 @pytest.fixture
 def in_dbcon(in_inst):
@@ -12,6 +14,7 @@ def in_dbcon(in_inst):
     return_val = oracleDbCon.OracleCloudDB()
     return_val.connect(in_inst)
     return return_val
+
 
 def test_crypto():
     assert 1 == 1
@@ -22,11 +25,12 @@ def test_connect_query(in_dbcon):
     my_sql = """
         select * from spriden where spriden_id = :id
     """
-    output = in_dbcon.execute_sql(my_sql, dict(id='@00287808'))
-    assert output['SPRIDEN_PIDM'][0] == 1165006
+    output = in_dbcon.execute_sql(my_sql, dict(id="@00287808"))
+    assert output["SPRIDEN_PIDM"][0] == 1165006
+
 
 def test_close_connection(in_dbcon):
     try:
         in_dbcon.close_connection()
     except Exception as err:
-        pytest.fail(f"Connection Close failed: {err}");
+        pytest.fail(f"Connection Close failed: {err}")
